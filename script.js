7a9b61de1f6c86f8273f2b0e101da5b48516bb9e
prycepavel <pryce.pavel@gmail.com>
2024-03-30 15:48:09 +0300

    Adds selection, purchase and sale of weapons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const xp = 0;
 let health = 100;
 let gold = 50;
-const currentWeapon = 0;
+let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 const inventory = ['stick'];
@@ -17,6 +17,12 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 },
+];
 
 const locations = [
   {
@@ -85,7 +91,25 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      const newWeapon = weapons[currentWeapon].name;
+      text.innerText = 'You now have a ' + newWeapon + '.';
+      inventory.push(newWeapon);
+      text.innerText += ' In your inventory you have: ' + inventory;
+    } else {
+      text.innerText = 'You do not have enough gold to buy a weapon.';
+    }
+  } else {
+    text.innerText = 'You already have the most powerful weapon!';
+    button2.innerText = 'Sell weapon for 15 gold';
+    button2.onclick = sellWeapon;
+  }
+}
 
 function fightSlime() {}
 
